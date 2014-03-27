2b5cab136de2524ef5db2b235a3d639048a3bd13
Isaac Zimmern <isaaczimmern@coral-area.local>
2014-03-26 18:04:48 -0700

    input values

--- app.rb
+++ app.rb
@@ -6,11 +6,18 @@ require ::File.expand_path('../config/environment',  __FILE__)
 set :app_file, __FILE__
 
 get '/' do
-  posts = Post.reverse_order(:created_at).all
-  erb :index, :locals => {:posts => posts}
+  erb :index
+end
+
+get '/categories' do
+  erb :categories
 end
 
 post '/posts' do
-  Post.create(:body => params[:body])
-  redirect to('/')
+ puts params
+ puts "KTSF"
+ Post.create(:description => params[:description], :name => params[:name], :contact => params[:contact],  :grade => params[:grade])
+ redirect to('/')
 end
+
+
--- db/migrate/20140317174627_create_posts.rb
+++ db/migrate/20140317174627_create_posts.rb
@@ -3,10 +3,10 @@ Sequel.migration do
   change do
   	create_table :posts do
       primary_key :id
-      string :name, :null => false
-      string :contact_info, :null => false
-      integer :grade, :null => false
       text :description, :null => false
+      string :name, :null => false
+      string :contact, :null => false
+      integer :grade, :null => false
       datetime :created_at, :null => false
       datetime :updated_at, :null => false
   	end
