6ddc568d0d173131cf076a19fa7dd01be6174fdc
Ilya Verzhbinsky <ilya.verz@gmail.com>
2014-04-14 18:09:20 -0700

    I love potatoes.

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ require ::File.expand_path('../config/environment',  __FILE__)
 set :app_file, __FILE__
 
 get '/' do
-  erb :index
+  posts = Post.reverse_order(:created_at).all
+  erb :index, :locals => {:posts => posts}
 end
 
 get '/categories' do
@@ -14,10 +15,20 @@ get '/categories' do
 end
 
 post '/posts' do
- puts params
- puts "KTSF"
- Post.create(:description => params[:description], :name => params[:name], :contact => params[:contact],  :grade => params[:grade])
- redirect to('/')
+  if params[:description].empty?
+    redirect to('/?error=1')
+  elsif params[:name].empty?
+    redirect to('/?error=1')
+
+  elsif params[:contact].empty?
+    redirect to('/?error=1')
+
+  elsif params[:grade].empty?
+    redirect to('/?error=1')
+  else
+    Post.create(:description => params[:description], :name => params[:name], :contact => params[:contact],  :grade => params[:grade])
+    redirect to('/?success=1')
+  end
 end
 
 
